8d4204bfaf30c46c2a2d5a87f15dbf3027ae035e
meriem23 <esra.msallem@gmail.com>
2020-06-20 12:41:08 +0100

    cart update

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -8,7 +8,7 @@ function Cart() {
                 <img className="CartImg" src='/images/Cart.Img.jpg' alt="cart banner" />
            
             <div className="MyContainer ">
-                your cart list
+            <img src="https://www.pngkit.com/png/detail/882-8824559_empty-cart-png-onthel-to-work.png" alt="empty cart" width="400" height="400"/>
             </div>
         </div>
     )
